6731e5349d05ead99d6c54580591ea5ca4fed35d
Alpkan Cicek <alpkan.cicek@tesodev.com>
2020-09-18 01:59:15 +0300

    ###

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
+const urlRouter = require('./routes/url');
 const indexRouter = require('./routes/index');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/url', urlRouter);
 app.use('/', indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,73 +1,31 @@
-const express = require('express');
-const router = express.Router();
-const NoSQL = require('nosql');
-const path = require('path');
-const db = NoSQL.load(path.join(__dirname, '../db.nosql'));
-const { v4: uuidv4 } = require('uuid');
+const Router = require('express').Router;
+const router = Router();
+const dns = require('dns');
 
-router.get('/url', function(req, res, next) {
-  db.find().make(function(builder) {
-    builder.callback(function(err, response) {
-      res.json(response);
-    });
-  });
+const hosts = {};
+
+router.get('', function(req, res, next) {
+  const host = handleHost(req.hostname, req.connection.remoteAddress.split(':')[req.connection.remoteAddress.split(':').length - 1]);
+  console.log(hosts);
+  res.json({host});
 });
 
-router.get('/url/:id', function(req, res, next) {
-  db.find().make(function(builder) {
-    builder.where('id', req.params.id);
-    builder.callback(function(err, response) {
-      if (response.length !== 0){
-        res.json(response[0]);
-      }
-      else {
-        res.send(404);
-      }
+const handleHost = function(hostname, ip){
+  let host = hosts[ip];
+  if (host){
+    return host;
+  } else {
+    dns.resolve4('google.com', (err, addresses) => {
+      if (err) throw err;
+      addresses.map(a => {
+        if (ip === a){
+          hosts[a] = hostname;
+          host = hostname;
+        }
+      });
     });
-  });
-});
-
-router.delete('/url/:id', function(req, res, next) {
-  db.remove().make(function(builder) {
-    builder.where('id', req.params.id);
-    builder.callback(function(err, count) {
-      console.log('removed documents:', count);
-      if (count !== 0){
-        res.send(200);
-      }
-      else {
-        res.send(404);
-      }
-    });
-  });
-});
-
-router.post('/url', function(req, res, next) {
-  if (!req.body.url || !req.body.title){
-    res.send(400);
-    return;
+    return host;
   }
-  db.insert({ id: uuidv4(), title: req.body.title, data: { url: req.body.url, voteCount: 0 }, createdAt: new Date(), updatedAt: new Date() });
-  res.send(201);
-});
-
-router.put('/url', function(req, res, next) {
-  if (!req.body.url || !req.body.title || !req.body.voteCount || !req.headers.urlid){
-    res.send(400);
-    return;
-  }
-  db.modify({ title: req.body.title, data: { url: req.body.url, voteCount: req.body.voteCount }, updatedAt: new Date() }).make(function(builder) {
-    builder.where('id', req.headers.urlid);
-    builder.callback(function(err, count) {
-      console.log('updated documents:', count);
-      if (count === 0){
-        res.send(404);
-      }
-      else {
-        res.send(200);
-      }
-    });
-  });
-});
+}
 
 module.exports = router;
